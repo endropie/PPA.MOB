50c90d7b75eee4c235e2fa6fe28ed0628e0eef7d
endropie <hendrowibowo@msn.com>
2020-07-30 00:41:38 +0700

    fix(index): add setting (host)

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import axios from 'axios'
 import qs from 'qs'
+import { LocalStorage } from 'quasar'
 
 const baseURL = () => {
+  if (LocalStorage.getItem('API-HOST')) return LocalStorage.getItem('API-HOST')
   if (!process.env.DEV) return 'http://10.20.10.2:8000'
   return `${location.protocol}//${location.hostname}:8000`
 }
@@ -10,8 +12,19 @@ const axiosInstance = axios.create({
   paramsSerializer: params => {
     return qs.stringify(params, { arrayFormat: 'repeat' })
   },
-  baseURL: baseURL()
+  baseURL: baseURL(),
+  optBaseURL: ['http://192.168.1.8:8000', 'http://10.20.10.2:8000'],
+  setBaseURL: (host) => {
+    console.warn('SET', host)
+    LocalStorage.set('API-HOST', host)
+    return axiosInstance.defaults.baseURL = host
+  }
 })
+
+axiosInstance.setBaseURL = (host) => {
+  LocalStorage.set('API-HOST', host)
+  return axiosInstance.defaults.baseURL = host
+}
 
 axiosInstance.set = (method, url, data) => {
   if (method.toUpperCase() === 'POST') return axiosInstance.post(url, data)
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,14 +12,7 @@
       transition-prev="slide-right"
       transition-next="slide-left"
     >
-      <!-- <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
-      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
-      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
-      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" /> -->
-
       <q-carousel-slide :name="1" img-src="~/assets/images/home-1.jpg" />
-      <!-- <q-carousel-slide :name="2" img-src="~/assets/images/home-2.jpg" />
-      <q-carousel-slide :name="3" img-src="~/assets/images/home-3.jpg" /> -->
       <q-carousel-slide :name="2" img-src="~/assets/images/pot-1.jpg" />
       <q-carousel-slide :name="3" img-src="~/assets/images/pot-2.jpg" />
       <q-carousel-slide :name="4" img-src="~/assets/images/pot-3.jpg" />
@@ -29,13 +22,35 @@
           position="bottom"
           :offset="[50, 50]"
         >
-        <div class="row justify-center">
-          <q-btn glossy rounded color="red-14" size="lg" :label="$t('auth.login.title')" @click="loginForm()" v-if="login"/>
+        <div class="column justify-center q-gutter-sm">
+
+          <q-btn glossy color="red-14" size="lg" label="L O G I N" @click="loginForm()" v-if="login"/>
+          <q-btn glossy color="blue-grey" size="lg" :label="'Configuration'" @click="configDialog = true" />
+
         </div>
         </q-carousel-control>
       </template>
 
     </q-carousel>
+    <q-dialog v-model="configDialog" persistent transition-show="scale" transition-hide="scale">
+      <q-card style="width: 300px">
+        <q-card-section class="bg-teal text-white q-pa-sm">
+          <div class="text-h6">Configuration</div>
+        </q-card-section>
+        <q-card-section class=" q-pa-sm">
+          <q-select
+            label="API Host"
+            :value="$axios.defaults.baseURL"
+            :options="$axios.defaults.optBaseURL"
+            @input="setHostAPI"
+          />
+        </q-card-section>
+
+        <q-card-actions align="right" class="bg-white text-teal">
+          <q-btn flat label="Cancel" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 <script>
@@ -48,8 +63,10 @@ export default {
   data: () => ({
     login: false,
     slide: 1,
+    configDialog: false
   }),
   created() {
+    console.warn('BASE', this.$axios.defaults.baseURL)
     this.load()
   },
   methods: {
@@ -59,7 +76,7 @@ export default {
         else this.login = true
       }, 1000)
     },
-    loginForm() {
+    loginForm () {
       this.$app.modal({
         component: LoginComponent,
         parent: this,
@@ -75,6 +92,18 @@ export default {
       }).onDismiss(() => {
         console.log('Called on OK or Cancel')
       })
+    },
+    setHostAPI (v) {
+      this.$axios.defaults.setBaseURL(v)
+      this.configDialog = false
+      setTimeout(() => {
+        this.$q.notify('API Host changed to '+ v)
+      }, 500)
+    },
+    config () {
+      this.$axios.setBaseURL('localhost:8001')
+      console.warn('URL', this.$axios.defaults.baseURL)
+      // console.warn('CONFIG', this.$axios.defaults)
     }
   },
 }
